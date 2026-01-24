82492c96a9b5fde9e4a56a169a1575704477ed93
riz007 <rizwanulrudra@gmail.com>
2026-01-24 12:38:12 +0700

    fix: middleware issue debug

--- middleware.js
+++ middleware.js
@@ -1,23 +1,9 @@
 import { NextResponse } from "next/server";
-import { defaultLocale, locales } from "./i18n/routing";
 
-const hasLocalePrefix = (pathname) =>
-  locales.some(
-    (locale) => pathname === `/${locale}` || pathname.startsWith(`/${locale}/`),
-  );
-
-export function middleware(request) {
-  const { pathname } = request.nextUrl;
-
-  if (hasLocalePrefix(pathname)) {
-    return NextResponse.next();
-  }
-
-  const url = request.nextUrl.clone();
-  url.pathname = `/${defaultLocale}${pathname}`;
-  return NextResponse.redirect(url);
+export function middleware() {
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/", "/(en|bn|th|zh|de)/:path*"],
+  matcher: [],
 };
